9ed68258316989791f0cfe16275bd49c33fb1b8e
Willian <54459687+willTOP2@users.noreply.github.com>
2021-03-09 10:46:20 -0300

    Add files via upload

--- teste-maven-01/src/main/java/Calculos.java
+++ teste-maven-01/src/main/java/Calculos.java
@@ -1,4 +1,19 @@
 
 public class Calculos {
+	
+	double fosforo;
+	
+	
+	
+	void CalcFosforo(int n1, int n2) {
+		
+		if(n1 == 1) {
+			
+			fosforo = 9; 
+			
+		} else 
+			fosforo = 12;
+	}
+	
 
 }
