f813e2d959266501c76d8059c2a2428ef10775ea
Willian <willian.rodrygues146@gmail.com>
2021-04-24 15:52:32 -0300

    Create InterfaceCorrecaoNitrogenio.java
    Adicao da classe de interface

---
+++ src/main/java/InterfaceCorrecaoNitrogenio.java
@@ -0,0 +1,13 @@
+
+public interface InterfaceCorrecaoNitrogenio {
+
+
+    public double QuantidadeAplicada(double teorFosforo,double fosforo);
+
+   
+    
+    public double CorrecaoNitrogenio(double teorFosforo,  double fosforo);
+	
+	
+	
+}
