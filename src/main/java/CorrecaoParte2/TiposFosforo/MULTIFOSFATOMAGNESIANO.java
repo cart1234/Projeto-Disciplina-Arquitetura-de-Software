df3980d6a97e972e4b9b0d169e87d9f81bf8d6b7
Willian <willian.rodrygues146@gmail.com>
2021-04-25 21:38:43 -0300

    Adicao da classe
    Essa classe foi adicionada fora do enum, pois contem metodos com correcao diferentes

---
+++ src/main/java/CorrecaoParte2/TiposFosforo/MULTIFOSFATOMAGNESIANO.java
@@ -0,0 +1,39 @@
+
+public class MULTIFOSFATOMAGNESIANO {
+
+	
+
+
+        
+        public double QuantidadeAplicada(double teorFosforo,  double fosforo) {
+        	
+        	if(teorFosforo > 0 && fosforo >0) {
+
+         	return ((teorFosforo -fosforo)* 2 * 2.29 * 100 /70.0) * 100/ 18.0;
+        	} else 
+        		return 0.0;
+
+       }
+
+
+        
+        public double CorrecaoFosforoEnxofre(  double teorFosforo, double fosforo) {
+            return QuantidadeAplicada(teorFosforo, fosforo) * 0.11;
+
+        }
+
+       
+        public double CorrecaoFosforoCalcio(  double teorFosforo, double fosforo) {
+            return QuantidadeAplicada(teorFosforo, fosforo) * 0.18 ;
+
+        }
+
+
+
+
+
+   
+	
+	
+	
+}
