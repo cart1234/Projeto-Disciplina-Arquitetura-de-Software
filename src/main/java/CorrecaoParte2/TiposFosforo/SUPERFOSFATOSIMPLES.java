882b76e71ae5b3de7c2d8a8e198543349b25687d
Willian <willian.rodrygues146@gmail.com>
2021-04-25 21:38:57 -0300

    Adicao da classe
    Essa classe foi adicionada fora do enum, pois contem metodos com correcao diferentes

---
+++ src/main/java/CorrecaoParte2/TiposFosforo/SUPERFOSFATOSIMPLES.java
@@ -0,0 +1,44 @@
+
+public class SUPERFOSFATOSIMPLES {
+
+
+	
+	  
+    	  
+        public double QuantidadeAplicada(double teorFosforo,  double fosforo) { 
+            
+        	if(teorFosforo > 0 && fosforo > 0) {
+        		
+        	return ((teorFosforo -fosforo)* 2 * 2.29 * 100 /70.0) * 100/ 18.0;
+        	
+        	} else
+        		return 0.0;
+        			
+        }
+
+
+        
+        public double CorrecaoFosforoEnxofre(double teorFosforo,double fosforo) {
+            return QuantidadeAplicada(teorFosforo, fosforo)  * 0.1;
+        }
+
+       
+        public double CorrecaoFosforoCalcio( double teorFosforo,  double fosforo) {
+            return QuantidadeAplicada(teorFosforo,  fosforo) * 0.28 ;
+
+        }
+        
+        
+
+        public  double CustoCorrecaoFosforo(double SuperFosfatoSimples, double fosforo, double teorFosforo){
+
+            return SuperFosfatoSimples * (this.QuantidadeAplicada(teorFosforo,fosforo)  * 2.42 /1000.00)/2.42;
+
+
+        }
+    
+
+	
+	
+	
+}
